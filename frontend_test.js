bdd7e711da4bb94db1fb2b54f2d303c1de68d148
Unknown <patrick.bonn@yahoo.de>
2018-01-11 15:56:02 +0100

    a lot

--- frontend_test.js
+++ frontend_test.js
@@ -1,6 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById('test_input').addEventListener("submit", function(e){
-    e.preventDefault();
+ document.addEventListener("DOMContentLoaded", function() {
+
+  document.getElementById('test_input').addEventListener("submit", function(e){			//bedeutet (geh ins dokument nimm das Element(ID)welches test_input heist)
+    e.preventDefault();					// bedeutet lad die seite nicht neu wie bei html sondern 
     console.log( checkUsername(e.target[0].value) );
+    console.log( checkEmail(e.target[1].value) );
+    console.log( checkPassword(e.target[2].value) );
+    console.log( checkPassword(e.target[5].value) );
+    //console.log(e);
+    //console.log(this);    // das bedeutet das ALLES angezeigt wird
+
   });
 });
+
--- tests.js
+++ tests.js
@@ -1,6 +1,25 @@
 const checkUsername = (user) => {
   return /^[a-zA-Z]+$/.test(user)
 }
-
-console.log("Username is just letters: ",  checkUsername("Tommy"))
+const checkEmail = (user) => {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(user)
+}
+//console.log("Username is just letters: ",  checkUsername("Tommy"))
 // hallo
+
+const checkPassword = (user) => {
+	return /^((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,}))/.test(user)
+
+}
+
+const checkAge = (user) => {
+	//return /^(?:[1-9]\d{2,}+|[2-9]\d|1[89])$/.test(user)
+}//EINS SUCHEN DAS GEHT REGEX OVER 18
+
+
+// so das hier unten weil es länger wird 
+// das hinter dem return bedeutet er prüft
+// die dort stehenden bedingungen auf ihre
+// richtigkeit, z.B. checkPassword muss
+// 1 Großbuchstaben, 1 Zahl, 1 Sonderzeichen(!)
+// und es muss mindestens 8 Zeichen lang sein 
